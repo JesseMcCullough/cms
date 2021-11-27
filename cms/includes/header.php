8abab8c9b885b2170f4f6dbf79ef16e5dac4c718
Jesse McCullough <jesse@jessem.dev>
2021-11-26 23:02:42 -0500

    Added a basic structure for the Pages UI.

---
+++ cms/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+	<meta charset="UTF-8" />
+	<link href="images/favicon.ico" type="image/x-icon" rel="icon" />
+	<link href="styles/style.css" type="text/css" rel="stylesheet" />
+	<title><?php echo $pageTitle; ?> | CMS</title>
+</head>
+<body>
+<!-- TO-DO: Add side navigation bar. -->
+<!-- TO-DO: Add content area. -->
---
+++ cms/pages.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Pages";
+require_once("includes/header.php");
+?>
+
+<!-- TO-DO: Add a button for creating a new page. -->
+<!-- Display each page that is in the /pages folder. --> 
+
+<?php require_once("includes/footer.php"); ?>
