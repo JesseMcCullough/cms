5da595254a1120855478296376ee48fc271d7b7d
Jesse McCullough <jesse@jessem.dev>
2026-06-05 18:46:47 -0400

    Add Page and API router to Express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+import express from "express";
+import apiRouter from "./routes/api/index.js";
+import pageRouter from "./routes/page.js";
+import database from "./database/database.js";
+
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use("/api", apiRouter);
+app.use("/pages", pageRouter);
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-});
-
-app.get("/test", (req, res) => {
-    res.send("Received test request");
 });
