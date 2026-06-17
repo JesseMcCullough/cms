7a5695a5312a20c05b1fe1bf0cf77334a0ca4c66
Jesse McCullough <jesse@jessem.dev>
2026-06-17 13:42:36 -0400

    Add content validator against section schema

---
+++ src/validators/contentValidator.js
@@ -0,0 +1,81 @@
+export function validateContent(content, schema) {
+    /**
+     * content: {
+     *      fieldName: value
+     * }
+     *
+     * schema: { // schema for whole section, should get schema for that content field
+     *      fieldName: {
+     *          property: value
+     *      }
+     * }
+     */
+    // loop through content and run against schema, making sure each content field exists
+    // needed to check content against fields that aren't defined in the schema
+    for (const fieldName of Object.keys(content)) {
+        const fieldSchema = schema[fieldName];
+
+        if (!fieldSchema) {
+            throw new Error(`${fieldName} doesn't exist in schema`);
+        }
+    }
+
+    for (const fieldName of Object.keys(schema)) {
+        const schemaField = schema[fieldName];
+        const value = content[fieldName];
+
+        if (schemaField.required && value === undefined) {
+            throw new Error(`${fieldName} is required`);
+        }
+
+        // passed required check and no value, no further checks necessary
+        if (value === undefined) {
+            continue;
+        }
+
+        switch (schemaField.type) {
+            case "string":
+                if (typeof value !== "string") {
+                    throw new Error(`${fieldName} must be a string`);
+                }
+
+                const minLength = schemaField.minLength;
+                if (minLength !== undefined && value.length < minLength) {
+                    throw new Error(
+                        `${fieldName} must be at least ${minLength} characters`,
+                    );
+                }
+
+                const maxLength = schemaField.maxLength;
+                if (maxLength !== undefined && value.length > maxLength) {
+                    throw new Error(
+                        `${fieldName} must be no more than ${maxLength} characters`,
+                    );
+                }
+
+                break;
+            case "boolean":
+                if (typeof value !== "boolean") {
+                    throw new Error(`${fieldName} must be a boolean`);
+                }
+
+                break;
+            case "number":
+                if (typeof value !== "number") {
+                    throw new Error(`${fieldName} must be a number`);
+                }
+
+                const min = schemaField.min;
+                if (min !== undefined && value < min) {
+                    throw new Error(`${fieldName} must be at least ${min}`);
+                }
+
+                const max = schemaField.max;
+                if (max !== undefined && value > max) {
+                    throw new Error(`${fieldName} must be no more than ${max}`);
+                }
+
+                break;
+        }
+    }
+}
