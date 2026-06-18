39ae99aca066f627ffae223d763f6a9e21a1c905
Jesse McCullough <jesse@jessem.dev>
2026-06-18 14:36:27 -0400

    Add getFields to section model and improve parameter validation for create and update

--- src/models/section.js
+++ src/models/section.js
@@ -1,16 +1,43 @@
 import database from "#database";
 import AppError from "#apperror";
+import { validateSchema } from "#validators/schemaValidator";
 
 // probably need to do some kind of stringify thingy with the fields
 
 export function create(name, title, fields) {
+    /**
+     * {
+     *      "title": "",
+     *      "name": "",
+     *      "fields": {
+     *          "something": {
+     *              type: "string"
+     *          },
+     *      },
+     * }
+     */
+
+    if (typeof name !== "string" || !name.trim()) {
+        throw AppError.badRequest("'name' is required'");
+    }
+
+    if (typeof title !== "string" || !title.trim()) {
+        throw AppError.badRequest("'title' is required'");
+    }
+
+    if (typeof fields !== "object") {
+        throw AppError.badRequest("'fields' is required'");
+    }
+
+    validateSchema(fields);
+
     const insert = database.prepare(
         "INSERT INTO sections (name, title, fields) VALUES (?, ?, ?)",
     );
 
     try {
         console.log(name, title, fields);
-        insert.run(name, title, fields);
+        insert.run(name, title, JSON.stringify(fields));
     } catch (err) {
         if (err.errcode === 2067) {
             throw AppError.conflict("Section name already exists");
@@ -29,18 +56,32 @@ export function update(id, data) {
     const values = [];
 
     if (data.name !== undefined) {
+        if (typeof data.name !== "string" || !data.name.trim()) {
+            throw AppError.badRequest("'name' must be a string'");
+        }
+
         updates.push("name = ?");
         values.push(data.name);
     }
 
     if (data.title !== undefined) {
+        if (typeof data.title !== "string" || !data.title.trim()) {
+            throw AppError.badRequest("'title' must be a string'");
+        }
+
         updates.push("title = ?");
         values.push(data.title);
     }
 
     if (data.fields !== undefined) {
+        if (typeof data.fields !== "object") {
+            throw AppError.badRequest("'fields' must be an object'");
+        }
+
+        validateSchema(data.fields);
+
         updates.push("fields = ?");
-        values.push(data.fields);
+        values.push(JSON.stringify(data.fields));
     }
 
     if (updates.length === 0) {
@@ -81,4 +122,17 @@ export function remove(id) {
     return {
         success: true,
     };
+}
+
+export function getFields(id) {
+    const statement = database.prepare(
+        "SELECT fields FROM sections WHERE id = ?",
+    );
+    const fields = statement.get(id);
+
+    if (!fields) {
+        throw AppError.notFound("Section not found");
+    }
+
+    return JSON.parse(fields.fields);
 }
