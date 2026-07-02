67705372b9258f4fbcc6d3fc7939ffeee5833043
Jesse McCullough <jesse@jessem.dev>
2026-07-02 18:04:36 -0400

    Add create page form

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,10 @@
+import CreatePage from "@/CreatePage";
+
 export default function App() {
     return (
         <>
             <h1>Hello worldddddddddddddddddddd</h1>
+            <CreatePage />
         </>
     );
 }
